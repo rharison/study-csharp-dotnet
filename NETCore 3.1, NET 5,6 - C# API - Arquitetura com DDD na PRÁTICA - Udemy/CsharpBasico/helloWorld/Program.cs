d09e6ce9d426ae705aeee01dbbab8f3f478a0766
@rharison <rharison.abreu@gmail.com>
2023-04-05 23:54:44 -0300

    feat: aulas

--- NETCore 3.1, NET 5,6 - C# API - Arquitetura com DDD na PRÁTICA - Udemy/CsharpBasico/helloWorld/Program.cs
+++ NETCore 3.1, NET 5,6 - C# API - Arquitetura com DDD na PRÁTICA - Udemy/CsharpBasico/helloWorld/Program.cs
@@ -6,7 +6,23 @@ namespace HelloWolrd
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int varialTeste;
+            bool VariavelBooleana = false;
+            varialTeste = 123;  
+
+            if(VariavelBooleana == false) {
+                varialTeste = 765; 
+            }
+
+            var TesteInteger = 111;
+            var TesteBool = true;
+            var TesteString = "Aula";
+
+            Console.WriteLine("O Valor da variável é {0}", varialTeste);
+            Console.WriteLine("O Valor da variável é {0}", TesteInteger);
+            Console.WriteLine("O Valor da variável é {0}", TesteBool);
+            Console.WriteLine("O Valor da variável é {0}", TesteString);
+
         }
     }
 }
