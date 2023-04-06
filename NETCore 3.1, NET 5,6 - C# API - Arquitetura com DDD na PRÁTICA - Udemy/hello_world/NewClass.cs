71047700d7ee00205ce0a724705771bf06ccf4fb
@rharison <rharison.abreu@gmail.com>
2023-04-05 22:36:31 -0300

    feat: hello world

---
+++ NETCore 3.1, NET 5,6 - C# API - Arquitetura com DDD na PRÁTICA - Udemy/hello_world/NewClass.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class ClasseExempo {
+    public static void Main() {
+        System.Console.WriteLine("Hello World");
+    }
+}
