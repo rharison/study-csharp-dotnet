ccb7e36e5cacbd87e9b52e94f7ac9781cbf29394
@rharison <rharison.abreu@gmail.com>
2023-04-10 22:10:24 -0300

    feat: implementando swagger

--- NETCore 3.1, NET 5,6 - C# API - Arquitetura com DDD na PRÁTICA - Udemy/curso_api_netcore/src/Api.Application/Startup.cs
+++ NETCore 3.1, NET 5,6 - C# API - Arquitetura com DDD na PRÁTICA - Udemy/curso_api_netcore/src/Api.Application/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace application
 {
@@ -29,6 +30,23 @@ namespace application
             ConfigureService.ConfigureDependenciesService(services);
             ConfigureRepository.ConfigureDependenciesRepository(services);
             services.AddControllers();
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo {
+                    Version = "v1",
+                    Title = "Curso de API com AspNetCore 3.1 - Na Prática",
+                    Description = "Arquitetura DDD",
+                    TermsOfService = new Uri("http://www.mfrinfo.com.br"),
+                    Contact = new OpenApiContact {
+                        Name = "Rharison Lucas Moreira Abreu",
+                        Email = "rharison.abreu@gmail.com",
+                        Url = new Uri("http://www.rharison.com")
+                    },
+                    License = new OpenApiLicense {
+                        Name = "Termo de licença de uso",
+                        Url = new Uri("http://www.rharison.com")
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,7 +56,11 @@ namespace application
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Curso de API com AspNetCore 3.1");
+                c.RoutePrefix = string.Empty;
+            });
             app.UseRouting();
 
             app.UseAuthorization();
